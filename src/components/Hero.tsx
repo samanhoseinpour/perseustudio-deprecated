7c3809d4079bdeca075ec7f48333d04fdbc01f61
Saman <samangithoseinpour@gmail.com>
2024-03-14 07:09:57 +0330

    autoPlay fixed on mobile devices

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <header className="full-bleed relative z-10 flex h-screen items-center justify-end bg-cover bg-center pb-20 max-sm:h-full">
-      <video muted loop autoPlay>
+      <video muted loop playsInline autoPlay>
         <source src="/img/hero.mov" />
       </video>
     </header>
